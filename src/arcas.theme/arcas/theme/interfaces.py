b8601f8ee34e75222f9ea3d56225fe64b3c10d21
pablo.musa <pablo.musa@gmail.com>
2014-10-10 02:17:33 +0000

    actualizando el tema

--- src/arcas.theme/arcas/theme/interfaces.py
+++ src/arcas.theme/arcas/theme/interfaces.py
@@ -1,5 +1,8 @@
 __author__ = 'Paul'
 from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
+
 class IArcasTheme(Interface):
     """Marker interface that defines a ZTK browser layer.
     """
+

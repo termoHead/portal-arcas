b07ba1a4cd23bacae9c5d0ecefe4a0778166b1fc
pablo.musa <pablo.musa@gmail.com>
2014-10-10 14:34:41 +0000

    Update Logo

---
+++ src/arcas.theme/arcas/theme/common.py
@@ -0,0 +1,76 @@
+from cgi import escape
+from datetime import date
+from urllib import unquote
+
+from plone.memoize.view import memoize
+from zope.component import getMultiAdapter
+from zope.deprecation.deprecation import deprecate
+from zope.i18n import translate
+from zope.interface import implements, alsoProvides
+from zope.viewlet.interfaces import IViewlet
+
+from AccessControl import getSecurityManager
+from Acquisition import aq_base, aq_inner
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safe_unicode
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFPlone import PloneMessageFactory as _
+
+from plone.app.layout.globals.interfaces import IViewView
+
+
+class ViewletBase(BrowserView):
+    """ Base class with common functions for link viewlets.
+    """
+    implements(IViewlet)
+
+    def __init__(self, context, request, view, manager=None):
+        super(ViewletBase, self).__init__(context, request)
+        self.__parent__ = view
+        self.context = context
+        self.request = request
+        self.view = view
+        self.manager = manager
+
+    @property
+    @deprecate("Use site_url instead. ViewletBase.portal_url will be removed in Plone 4")
+    def portal_url(self):
+        return self.site_url
+
+    def update(self):
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.site_url = self.portal_state.portal_url()
+        self.navigation_root_url = self.portal_state.navigation_root_url()
+
+    def render(self):
+        # defer to index method, because that's what gets overridden by the template ZCML attribute
+        return self.index()
+
+    def index(self):
+        raise NotImplementedError(
+            '`index` method must be implemented by subclass.')
+
+
+
+
+class LogoViewlet(ViewletBase):
+    index = ViewPageTemplateFile('browser/logo.pt')
+
+    def update(self):
+        super(LogoViewlet, self).update()
+
+        portal = self.portal_state.portal()
+        bprops = portal.restrictedTraverse('base_properties', None)
+        if bprops is not None:
+            logoName = bprops.logoName
+        else:
+            logoName = 'logo.jpg'
+
+        logoTitle = self.portal_state.portal_title()
+        self.logo_tag = portal.restrictedTraverse(logoName).tag(title=logoTitle, alt=logoTitle)
+        self.navigation_root_title = self.portal_state.navigation_root_title()
+
+

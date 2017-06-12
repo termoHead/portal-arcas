7431f20fc25df3a86cb692a88c6a2f72587ce30b
Pablo <termo_head@yahoo.com>
2017-06-12 12:09:20 -0300

    pantalla final formularios

---
+++ src/arcas.content/arcas/content/formsOk_view.py
@@ -0,0 +1,24 @@
+# We must use BrowserView from view, not from zope.browser
+from Products.Five.browser import BrowserView
+
+class FormsOkView(BrowserView):
+
+    def __init__(self, context, request):
+        """ Initialize context and request as view multi adaption parameters.
+
+        Note that the BrowserView constructor does this for you.
+        This step here is just to show how view receives its context and
+        request parameter. You do not need to write __init__() for your
+        views.
+        """
+        self.context = context
+        self.request = request
+
+    # by default call will call self.index() method which is mapped
+    # to ViewPageTemplateFile specified in ZCML
+    #def __call__():
+    #
+    def mensaje(self):        
+        miurl=self.context.REQUEST.URL+"/formsOk_view?mensaje=Perfecto!"            
+        dato=self.request.form["mensaje"]
+        return dato
